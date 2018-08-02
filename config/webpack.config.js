ae5887d80ccd6c4a7a1487299110311b933ed322
HANATANI Takuma <mail@potato4d.me>
2018-08-02 22:29:07 +0900

    update webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -75,7 +75,6 @@ module.exports = {
 
 if (!isDev) {
   module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = [
     new VueLoaderPlugin(),
     new webpack.EnvironmentPlugin([
@@ -84,12 +83,6 @@ if (!isDev) {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
       }
     }),
     new webpack.LoaderOptionsPlugin({
