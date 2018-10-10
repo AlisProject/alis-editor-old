f98d8701cfb4b5d280b421c06ef45604b637c7d6
HANATANI Takuma <mail@potato4d.me>
2018-10-10 14:13:35 +0900

    add npmignore

--- getHash.js
+++
@@ -1,9 +0,0 @@
-const _exec = require('child_process').exec
-const { promisify } = require('util')
-const exec = promisify(_exec)
-
-;(async () => {
-  const releaseHash = (await exec('git rev-parse HEAD')).stdout
-  await exec(`git checkout master`)
-  await exec(`git cherry-pick ${releaseHash}`)
-})()
--- release.sh
+++
@@ -1,9 +0,0 @@
-git branch -D release
-git checkout -b release
-mv .gitignore ._gitignore
-rm -rf dist
-yarn build
-git add -A
-git commit -m 'build'
-yarn standard-version
-node getHash.js
