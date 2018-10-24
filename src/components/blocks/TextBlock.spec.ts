756ef6dedd1f7cad929cb8aede9fcfc01969ec10
y-temp4 <y.temp4@gmail.com>
2018-10-24 13:00:20 +0900

    Add preview feature to HeadingBlock and TextBlock

--- src/components/blocks/TextBlock.spec.ts
+++ src/components/blocks/TextBlock.spec.ts
@@ -1,4 +1,4 @@
-import { mount,config } from '@vue/test-utils'
+import { mount, config } from '@vue/test-utils'
 import TextBlock from './TextBlock.vue'
 import * as BlockTypes from '../../types/Blocks'
 config.logModifiedComponents = false
@@ -15,6 +15,24 @@ describe('TextBlock.vue', () => {
     const wrapper = mount(TextBlock, {
       propsData: {
         block
+      },
+      stubs: ['ShadowInput']
+    })
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  test('renders correctly (preview mode)', () => {
+    const block: BlockTypes.TextBlock = {
+      id: '123',
+      type: BlockTypes.BlockType.Text,
+      payload: {
+        body: 'foo'
+      }
+    }
+    const wrapper = mount(TextBlock, {
+      propsData: {
+        block,
+        preview: true
       },
       stubs: ['ShadowInput']
     })
