02c5e0efb354b7b6868acc50c3343b2f49c5e138
y-temp4 <y.temp4@gmail.com>
2018-10-24 16:34:22 +0900

    Add test of EmbedBlock

--- src/components/blocks/EmbedBlock.spec.ts
+++ src/components/blocks/EmbedBlock.spec.ts
@@ -21,4 +21,61 @@ describe('EmbedBlock.vue', () => {
     })
     expect(wrapper.element).toMatchSnapshot()
   })
+
+  test('renders twitter profile content correctly', () => {
+    const block: BlockTypes.EmbedBlock = {
+      id: '123',
+      type: BlockTypes.BlockType.Embed,
+      payload: {
+        src: 'https://twitter.com/ALIS_media'
+      },
+      children: []
+    }
+    const wrapper = mount(EmbedBlock, {
+      propsData: {
+        block
+      },
+      stubs: ['ShadowInput']
+    })
+    wrapper.setData({
+      html: `<div data-alis-iframely-url="https://twitter.com/alis_media" contenteditable="false">
+        <a href="https://twitter.com/alis_media" target="_blank" class="twitter-profile-card">
+          <div class="title">ALIS (@ALIS_media) | Twitter</div><div class="description">
+            The latest Tweets from ALIS (@ALIS_media). ALIS is social media platform using blockchain. We help people to find trustworthy articles and people quickly.
+            ALIS公式アカウントです。日々の活動状況やニュースを発信します。#ALIS $ALIS. Japan
+          </div>
+          <div class="site">twitter.com</div>
+        </a>
+      </div>`
+    })
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  test('renders OGP content correctly', () => {
+    const block: BlockTypes.EmbedBlock = {
+      id: '123',
+      type: BlockTypes.BlockType.Embed,
+      payload: {
+        src: 'https://alis.to'
+      },
+      children: []
+    }
+    const wrapper = mount(EmbedBlock, {
+      propsData: {
+        block
+      },
+      stubs: ['ShadowInput']
+    })
+    wrapper.setData({
+      html: `<div data-alis-iframely-url="https://alis.to" contenteditable="false">
+        <a href="https://alis.to" target="_blank" class="iframely-embed-card">
+          <div class="title">ALIS | 信頼できる記事と人々を明らかにする全く新しいソーシャルメディア</div>
+          <div class="description">クリプトの人気記事一覧</div>
+          <img src="https://alis.to/d/nuxt/dist/touch-icon.png" class="thumbnail">
+          <div class="site">alis.to</div>
+        </a>
+      </div>`
+    })
+    expect(wrapper.element).toMatchSnapshot()
+  })
 })
